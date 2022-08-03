120b9987e41063480ef438a5607c1074df6b6a51
Alistair Saldanha <shaanalistair47@gmail.com>
2022-08-03 19:33:59 +0530

    Tested Accordian and Toast

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,6 @@
-import { IonButton, IonButtons, IonCard, IonCardContent, IonCardSubtitle, IonCol, IonContent, IonFooter, IonHeader, IonIcon, IonInput, IonItem, IonItemDivider, IonLabel, IonList, IonModal, IonPage, IonRow, IonTitle, IonToggle, IonToolbar, useIonActionSheet } from '@ionic/react'
+import { IonAccordion, IonAccordionGroup, IonButton, IonButtons, IonCard, IonCardContent, IonCardSubtitle, IonCol, IonContent, IonFooter, IonHeader, IonIcon, IonInput, IonItem, IonItemDivider, IonLabel, IonList, IonModal, IonPage, IonRow, IonTitle, IonToast, IonToggle, IonToolbar } from '@ionic/react'
 import React, { useState } from 'react'
-import { settingsOutline } from "ionicons/icons";
+import { settingsOutline, createOutline } from "ionicons/icons";
 import './Modal.css'
 
 function Modal() {
@@ -9,8 +9,40 @@ function Modal() {
     const [name, setName] = useState("");
     const [password, setPassword] = useState("");
     const [checked, setChecked] = useState(false);
-    const [present, dismiss] = useIonActionSheet();
+    const [showToast1, setShowToast1] = useState(false);
+    const [showToast2, setShowToast2] = useState(false);
+    const [showToast3, setShowToast3] = useState(false);
+    const [showToast4, setShowToast4] = useState(false);
 
+    const [checked1, setChecked1] = useState(false);
+    const [checked2, setChecked2] = useState(false);
+    const [checked3, setChecked3] = useState(false);
+    const [checked4, setChecked4] = useState(false);
+
+    const [changeName, setChangeName] = useState("username");
+    const [changeUsername, setChangeUsername] = useState("dummy");
+    const [editName, setEditName] = useState(false);
+    const [editUserName, setEditUserName] = useState(false);
+
+    // Toggle Code
+    const handleToggleChange1 = (e: any) => {
+        setChecked1(e.detail.checked);
+        setShowToast1(true);
+    }
+    const handleToggleChange2 = (e: any) => {
+        setChecked2(e.detail.checked);
+        setShowToast2(true);
+    }
+    const handleToggleChange3 = (e: any) => {
+        setChecked3(e.detail.checked);
+        setShowToast3(true);
+    }
+    const handleToggleChange4 = (e: any) => {
+        setChecked4(e.detail.checked);
+        setShowToast4(true);
+    }
+    const [text, setText] = useState<string>();
+    // ---
     return (
         <IonPage>
             <IonHeader>
@@ -85,7 +117,147 @@ function Modal() {
                         </IonCard>
                     </IonCol>
                 </IonRow>
-            </IonContent >
+                {/* <IonButton onClick={() => setShowToast1(true)} expand="block">Show Toast 1</IonButton> */}
+                {/* <IonButton onClick={() => setShowToast2(true)} expand="block">Show Toast 2</IonButton> */}
+                {/* <IonToast
+                    isOpen={showToast1}
+                    onDidDismiss={() => setShowToast1(false)}
+                    message="Your settings have been saved."
+                    duration={200}
+                />
+
+                <IonToast
+                    isOpen={showToast2}
+                    onDidDismiss={() => setShowToast2(false)}
+                    message="Are you sure you want to save the changes?"
+                    position="bottom"
+                    buttons={[
+                        {
+                            side: 'start',
+                            text: 'Discard',
+                            handler: () => {
+                                console.log('Favorite clicked');
+                            }
+                        },
+                        {
+                            side: 'end',
+                            text: 'Save Changes',
+                            handler: () => {
+                                console.log('Cancel clicked');
+                            }
+                        }
+                    ]}
+                /> */}
+                {/* <IonItem style={{ marginTop: "15px", width: "95%", fontFamily: "Montserrat-SB" }}>
+                    <IonInput type="text" placeholder="Change Name" value={changeName} onIonChange={(e: any) => { setChangeName(e.target.value) }}></IonInput>
+                    {editName ? (
+                        <IonButton style={{ height: "25px" }} onClick={() => setEditName(false)}>{"Save"}</IonButton>
+                    ) : (
+                        <IonIcon
+                            onClick={() => setEditName(true)}
+                            icon={createOutline}
+                            className="set-status"
+                            color="dark"
+                        ></IonIcon>
+                    )}
+                </IonItem> */}
+                <IonToast
+                    isOpen={showToast1}
+                    onDidDismiss={() => setShowToast1(false)}
+                    message={checked1 ? "Application Notifications has been enabled." : "Application Notifications has been disabled."}
+                    position="top"
+                    duration={250}
+                />
+                <IonToast
+                    isOpen={showToast2}
+                    onDidDismiss={() => setShowToast2(false)}
+                    message={checked2 ? "SMS and Email Notification has been enabled." : "SMS and Email Notification has been disabled."}
+                    position="top"
+                    duration={250}
+                />
+                <IonToast
+                    isOpen={showToast3}
+                    onDidDismiss={() => setShowToast3(false)}
+                    message={checked3 ? "Special Offer has been enabled." : "Special Offer has been disabled."}
+                    position="top"
+                    duration={250}
+                />
+                <IonToast
+                    isOpen={showToast4}
+                    onDidDismiss={() => setShowToast4(false)}
+                    message={checked4 ? "Read Reminder has been enabled." : "Read Reminder has been disabled."}
+                    position="top"
+                    duration={250}
+                />
+                <IonAccordionGroup expand='inset' multiple={true}>
+                    <IonAccordion value="first">
+                        <IonItem slot="header" color="light">
+                            <IonLabel>Notification Settings</IonLabel>
+                        </IonItem>
+                        <div className="ion-padding" slot="content">
+                            <div style={{ display: "flex", justifyContent: "space-between", margin: "10px 0", padding: "10px" }}>
+                                <IonLabel style={{ fontFamily: "Montserrat-SB" }}>Application Notifications</IonLabel>
+                                <IonToggle value="AppNotify" checked={checked1} onIonChange={(e: any) => handleToggleChange1(e)} />
+                            </div>
+                            <div style={{ display: "flex", justifyContent: "space-between", margin: "10px 0", padding: "10px" }}>
+                                <IonLabel style={{ fontFamily: "Montserrat-SB" }}>SMS and Email Notification</IonLabel>
+                                <IonToggle value="SMSEmailNotify" checked={checked2} onIonChange={(e: any) => handleToggleChange2(e)} />
+                            </div>
+                            <div style={{ display: "flex", justifyContent: "space-between", margin: "10px 0", padding: "10px" }}>
+                                <IonLabel style={{ fontFamily: "Montserrat-SB" }}>Special Offer</IonLabel>
+                                <IonToggle value="SplOffer" checked={checked3} onIonChange={(e: any) => handleToggleChange3(e)} />
+                            </div>
+                            <div style={{ display: "flex", justifyContent: "space-between", margin: "10px 0", padding: "10px" }}>
+                                <IonLabel style={{ fontFamily: "Montserrat-SB" }}>Read Reminder</IonLabel>
+                                <IonToggle value="ReadRemind" checked={checked4} onIonChange={(e: any) => handleToggleChange4(e)} />
+                            </div>
+                        </div>
+                    </IonAccordion>
+                    <IonAccordion value="second">
+                        <IonItem slot="header" color="light">
+                            <IonLabel>User Settings</IonLabel>
+                        </IonItem>
+                        <div className="ion-padding" slot="content">
+                            <div style={{ marginTop: "15px", fontFamily: "Montserrat-SB", display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                                <IonInput type="text" placeholder="Change Name" value={changeName} onIonChange={(e: any) => { setChangeName(e.target.value) }}></IonInput>
+                                {editName ? (
+                                    <IonButton style={{ height: "25px" }} onClick={() => setEditName(false)}>{"Save"}</IonButton>
+                                ) : (
+                                    <IonIcon
+                                        onClick={() => setEditName(true)}
+                                        icon={createOutline}
+                                        className="set-status"
+                                        color="dark"
+                                    ></IonIcon>
+                                )}
+                            </div>
+                            <div style={{ marginTop: "15px", fontFamily: "Montserrat-SB", display: "flex", justifyContent: "center", alignItems: "center" }}>
+                                <IonInput type="text" placeholder="Change Username" value={changeUsername} onIonChange={(e: any) => { setChangeUsername(e.target.value) }}></IonInput>
+                                {editUserName ? (
+                                    <IonButton style={{ height: "25px" }} onClick={() => setEditUserName(false)}>{"Save"}</IonButton>
+                                ) : (
+                                    <IonIcon
+                                        onClick={() => setEditUserName(true)}
+                                        icon={createOutline}
+                                        className="set-status"
+                                        color="dark"
+                                    ></IonIcon>
+                                )}
+                            </div>
+                        </div>
+                    </IonAccordion>
+                    <IonAccordion value="third">
+                        <IonItem slot="header" color="light">
+                            <IonLabel>Change Interest</IonLabel>
+                        </IonItem>
+                    </IonAccordion>
+                </IonAccordionGroup>
+            </IonContent>
+
+            <IonItemDivider>Input with clear button when there is a value</IonItemDivider>
+            <IonItem>
+                <IonInput value={text} placeholder="Enter Input" onIonChange={e => setText(e.detail.value!)} clearInput></IonInput>
+            </IonItem>
         </IonPage >
     )
 }
